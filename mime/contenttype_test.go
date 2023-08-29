b9249a089ef510515796323007d56e1a9561ba45
Florian Hübner <code@hub.lol>
2023-08-29 20:16:06 +0200

    extra tests

--- mime/contenttype_test.go
+++ mime/contenttype_test.go
@@ -1,4 +1,4 @@
-package contenttype
+package mime
 
 import (
 	"testing"
@@ -39,22 +39,52 @@ func Test(t *testing.T) {
 			provided: []string{"application/xhtml+xml", "text/html"},
 			expected: "text/html",
 		},
+		{
+			name:     "wildcard 1",
+			header:   firefoxHeader,
+			provided: []string{"application/json"},
+			expected: "application/json",
+		},
+		{
+			name:     "wildcard 3",
+			header:   "text/a;q=0.9,*/*;q=1.0",
+			provided: []string{"text/a", "text/b"},
+			expected: "text/b",
+		},
+		{
+			name:     "quality priority 1",
+			header:   firefoxHeader,
+			provided: []string{"application/xml", "image/avif"},
+			expected: "image/avif",
+		},
+		{
+			name:     "quality priority 2",
+			header:   firefoxHeader,
+			provided: []string{"application/xml", "application/json"},
+			expected: "application/xml",
+		},
+		{
+			name:     "quality priority 3",
+			header:   "text/a;q=0.9,text/b;q=1.0",
+			provided: []string{"text/a", "text/b"},
+			expected: "text/b",
+		},
 	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			mt, err := Negotiate(test.header, test.provided)
+			mt, err := Negotiate(firefoxHeader, test.provided)
 			if err != nil {
 				t.Logf("error=%++q\n", err)
 				t.Fail()
 			}
-			if mt.String() != test.expected {
+			if mt != test.expected {
 				t.Fail()
 			}
 			if t.Failed() {
 				t.Logf("header:   %++q\n", test.header)
 				t.Logf("provided: %++q\n", test.provided)
 				t.Logf("expected: %++q\n", test.expected)
-				t.Logf("actual:   %++q\n", mt.String())
+				t.Logf("actual:   %++q\n", mt)
 			}
 		})
 	}
